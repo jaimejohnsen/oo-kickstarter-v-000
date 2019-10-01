0133f1790ee79a660e042570f65276171f02720b
Jaime Johnsen <drivetraindiva@gmail.com>
2019-10-01 11:57:55 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,12 +7,16 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(project)#accepts a Project as an argument and stores it in a backed_projects array
-    @backed_projects << project#telling that Backer instance about on of it's backed projects
-    project.backers << self #adds the backer to the project's backers array.
+# accepts a Project as an argument and stores it in a backed_projects array
+  def back_project(project)
+    #telling that Backer instance about on of it's backed projects
+    @backed_projects << project
+    #adds the backer to the project's backers array.
+    project.backers << self
 #project is an instance of the Project class and calling the backers method on that instance that give you access to the backers array (backers attribute).
 #self is an instance of the Backer class, we are storing the backer associated with the project
-
+  end
+end
 
 #class Backer
 
